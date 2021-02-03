7efd2474836edda3b48de751a3a21d29bb673481
zhili <zhilishen@yufuid.com>
2021-02-03 18:21:22 +0800

    feat: 代码微调

--- index.js
+++ index.js
@@ -4,37 +4,38 @@ const { Toolkit } = require('actions-toolkit')
 Toolkit.run(async tools => {
   tools.log.debug('This is triggered by event:issue_comment')
   const { owner, repo, issue_number } = tools.context.issue;
-  console.log('tools.context.issue >>> ', tools.context.issue);
-  tools.log.debug('payload >>>>>>>>>>>>', tools.context.payload);
-  tools.github.issues
-    .createComment({
-      owner,
-      repo,
-      issue_number,
-      body: tools.context.payload.comment.body,
-    })
-    .then(() => {
-      tools.exit.success('Comment repeated')
-    })
-    .catch(() => {
-      tools.exit.failure('Something went wrong')
-    })
-}, { event: ['issue_comment'] })
+  tools.log.debug('tools.context.ref >>> ', tools.context.ref);
+  tools.log.debug('payload >>>>>>>>>>>> ', tools.context.payload);
+  tools.log.debug('tools.context >>> ', tools.context);
+  // tools.github.issues
+  //   .createComment({
+  //     owner,
+  //     repo,
+  //     issue_number,
+  //     body: tools.context.payload.comment.body,
+  //   })
+  //   .then(() => {
+  //     tools.exit.success('Comment repeated')
+  //   })
+  //   .catch(() => {
+  //     tools.exit.failure('Something went wrong')
+  //   })
+})
 
-Toolkit.run(async tools => {
-  tools.log.debug('This is triggered by event:pull_request.closed')
-  const { owner, repo, pull_number } = tools.context.pullRequest;
-  tools.log.debug('payload >>>>>>>>>>>>', tools.context.payload);
-  if (tools.context.payload.pull_request.merged) {
-    tools.github.git
-      .deleteRef(tools.context.repo({
-        ref: `heads/${tools.context.payload.pull_request.head.ref}`
-      }))
-      .then(() => {
-        tools.log.success(`Branch ${tools.context.payload.pull_request.head.ref} deleted!`)
-      })
-  }
-  tools.exit.failure('Something went wrong')
-}, { event: 'pull_request.closed' })
+// Toolkit.run(async tools => {
+//   tools.log.debug('This is triggered by event:pull_request.closed')
+//   const { owner, repo, pull_number } = tools.context.pullRequest;
+//   tools.log.debug('payload >>>>>>>>>>>>', tools.context.payload);
+//   if (tools.context.payload.pull_request.merged) {
+//     tools.github.git
+//       .deleteRef(tools.context.repo({
+//         ref: `heads/${tools.context.payload.pull_request.head.ref}`
+//       }))
+//       .then(() => {
+//         tools.log.success(`Branch ${tools.context.payload.pull_request.head.ref} deleted!`)
+//       })
+//   }
+//   tools.exit.failure('Something went wrong')
+// }, { event: 'pull_request.closed' })
 
 
