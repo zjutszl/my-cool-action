bf42c96488819d87f87e852137f33b44272ca83a
zhili <zhilishen@yufuid.com>
2021-02-05 12:43:52 +0800

    chore: update

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const { Toolkit } = require('actions-toolkit');
 Toolkit.run(async tools => {
   tools.log.debug('tools.context.payload >>> \n', tools.context.payload);
   if (tools.context.event === 'issue_comment' && tools.context.payload.action === 'created') {
-    await tools.github.issues.createComment({
-      ...tools.context.issue,
-      body: "comment received!!",
-    });
-    tools.exit.success('success')
+    if (/hello/.test(tools.context.payload.comment.body)) {
+      await tools.github.issues.createComment({
+        ...tools.context.issue,
+        body: "comment received!!",
+      });
+      tools.exit.success('comment success!')
+    }
   }
 })
   .catch(e => {
