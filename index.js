ac9b8db891d29f85999b7521a05dae041dbd927c
zhili <zhilishen@yufuid.com>
2021-02-05 11:46:21 +0800

    chore: update

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const { Toolkit } = require('actions-toolkit');
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
+  tools.log.debug('tools.context.payload >>> \n', tools.context.payload);
   if (tools.context.event === 'issue_comment' && tools.context.payload.action === 'created') {
-    // const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
     const params = {
       ...tools.context.issue,
-      body: "test",
+      body: 'test222',
     }
     tools.log.debug('params', params);
-    // await octokit.issues.createComment(params)
     await tools.github.issues.createComment(params);
     tools.exit.success('success')
   }
@@ -18,21 +17,3 @@ Toolkit.run(async tools => {
   .catch(e => {
     tools.exit.failure(e);
   })
-
-// Toolkit.run(async tools => {
-//   tools.log.debug('This is triggered by event:pull_request.closed')
-//   const { owner, repo, pull_number } = tools.context.pullRequest;
-//   tools.log.debug('payload >>>>>>>>>>>>', tools.context.payload);
-//   if (tools.context.payload.pull_request.merged) {
-//     tools.github.git
-//       .deleteRef(tools.context.repo({
-//         ref: `heads/${tools.context.payload.pull_request.head.ref}`
-//       }))
-//       .then(() => {
-//         tools.log.success(`Branch ${tools.context.payload.pull_request.head.ref} deleted!`)
-//       })
-//   }
-//   tools.exit.failure('Something went wrong')
-// }, { event: 'pull_request.closed' })
-
-
